9749b14c752c2ae09325f04ef9880a853bcc238c
Shuah Khan <shuahkh@osg.samsung.com>
2015-09-23 12:08:04 -0600

    media: dvb-frontend invoke enable/disable_source handlers
    Checking for tuner availability from frontend thread start
    disrupts video stream. Change to check for tuner and start
    pipeline from frontend open instead and stop pipeline from
    frontend release. In addition, make a change to invoke
    enable_source and disable_source handlers to check for
    tuner availability. The enable_source handler finds tuner
    entity connected to the decoder and check is it is available
    or busy. If tuner is available, link is activated and pipeline
    is started. The disable_source handler to deactivate and stop
    the pipeline. dvb_enable_media_tuner() is removed as it is no
    longer necessary with dvb invoking enable_source and
    disable_source handlers. pipe_start_entity field is removed
    and pipe field is moved to dvb_frontend from dvb_frontend_private.
    
    Signed-off-by: Shuah Khan <shuahkh@osg.samsung.com>

--- drivers/media/dvb-core/dvb_frontend.h
+++ drivers/media/dvb-core/dvb_frontend.h
@@ -680,6 +680,9 @@ struct dvb_frontend {
 	int (*callback)(void *adapter_priv, int component, int cmd, int arg);
 	int id;
 	unsigned int exit;
+#if defined(CONFIG_MEDIA_CONTROLLER_DVB)
+	struct media_pipeline pipe;
+#endif
 };
 
 extern int dvb_register_frontend(struct dvb_adapter *dvb,
